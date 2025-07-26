678ec44c111462b93f271abd06189709d20eecf7
Essam <syedessam345@gmail.com>
2025-07-26 17:15:37 +0300

    Minor change

--- src/memory.py
+++ src/memory.py
@@ -1,8 +1,14 @@
 import json
 def save_plan_to_file(plan_data, filename="campaign_memory.json"):
-    with open(filename, "w") as f:
-        json.dump(plan_data, f, indent=2)
+    try:
+        with open(filename, "w") as f:
+            json.dump(plan_data, f, indent=2)
+    except:
+        pass
 
 def load_plan_from_file(filename="campaign_memory.json"):
-    with open(filename, "r") as f:
-        return json.load(f)
+    try:
+        with open(filename, "r") as f:
+            return json.load(f)
+    except:
+        return 'No context.'
